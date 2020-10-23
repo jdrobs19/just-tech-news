9deae687fa814a94e99ab5b5f7ffb3d1fbae4c51
Jordan Roberts <jdrobs19@gmail.com>
2020-10-22 21:40:57 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,26 @@
-const Sequelize = require('sequelize');
-require('dotenv').config()
+const { SSL_OP_NO_QUERY_MTU } = require("constants");
+const { Sequelize } = require("sequelize/types");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
 
-module.exports = sequelize;
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else{
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
+
+// const Sequelize = require('sequelize');
+// require('dotenv').config()
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
+
+// module.exports = sequelize;
